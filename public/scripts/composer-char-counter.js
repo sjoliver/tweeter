df80c008e2af12611bd401c5f1277522219c4dae
Sophie <sophiejoliver19+1@gmail.com>
2021-10-06 00:11:55 +0000

    add styling to character counter: counter val updates as user types/deletes  -- when count is -ve, counter is displayed in red

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,2 +1,24 @@
 // Solely responsible for the character counter
 
+const maxCharCount = 140;
+
+// $(document).ready runs a callback when the DOM is ready to be manipulated with jQuery
+$(document).ready(function() {
+  
+
+$("#tweet-text").on("input", function(event) {
+  
+  const inputLength = $(this).val().length;
+  const charCount = maxCharCount - inputLength
+  $(".counter").val(charCount);
+
+  if (charCount < 0) { 
+    $(".counter").css("color", "red")
+  } else {
+    $(".counter").css("color", "#545149")
+  }
+
+});
+
+
+});
