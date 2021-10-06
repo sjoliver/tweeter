144a53627171e64dfbba58773c9ef8c1c66f0dcd
Sophie <sophiejoliver19+1@gmail.com>
2021-10-06 03:25:58 +0000

    refactor character counter function to use terniary syntax instead of if/else

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,21 +4,17 @@ const maxCharCount = 140;
 
 // $(document).ready runs a callback when the DOM is ready to be manipulated with jQuery
 $(document).ready(function() {
+
+  $("#tweet-text").on("input", function(event) {
+    
+    const inputLength = $(this).val().length;
+    const charCount = maxCharCount - inputLength;
+    $(".counter").val(charCount);
+
+    const colour = charCount < 0 ? "red" : "inherit";
+    
+    $(".counter").css("color", colour)
   
-
-$("#tweet-text").on("input", function(event) {
-  
-  const inputLength = $(this).val().length;
-  const charCount = maxCharCount - inputLength
-  $(".counter").val(charCount);
-
-  if (charCount < 0) { 
-    $(".counter").css("color", "red")
-  } else {
-    $(".counter").css("color", "#545149")
-  }
-
-});
-
+  });
 
 });
